5839d5bbb753b535c71f4c70d257ff12f16a0d2c
chaitanyapotti <chaitanya.potti@gmail.com>
2021-02-04 18:49:03 +0800

    comment sdk

--- examples/vue-app-open-login/src/OpenLoginSdk.js
+++ examples/vue-app-open-login/src/OpenLoginSdk.js
@@ -2,7 +2,7 @@ import TorusSdk from "@toruslabs/torus-direct-web-sdk";
 
 const OPEN_LOGIN_URL = "http://localhost:3000";
 
-let openLoginIframe = null;
+// let openLoginIframe = null;
 
 const torusdirectsdk = new TorusSdk({
   baseUrl: OPEN_LOGIN_URL,
@@ -14,35 +14,36 @@ const torusdirectsdk = new TorusSdk({
 torusdirectsdk.init({ skipInit: true });
 
 export async function triggerLogin() {
-  const args = {
-    typeOfLogin: "google",
-    verifier: "google-lrc",
-    clientId: "221898609709-obfn3p63741l5333093430j3qeiinaa8.apps.googleusercontent.com"
-  };
-  if (openLoginIframe?.contentWindow) {
-    openLoginIframe.contentWindow.postMessage({ method: "triggerLogin", args, domain: location.origin }, OPEN_LOGIN_URL);
-  }
-  return torusdirectsdk.triggerLogin(args);
+  // const args = {
+  //   typeOfLogin: "google",
+  //   verifier: "google-lrc",
+  //   clientId: "221898609709-obfn3p63741l5333093430j3qeiinaa8.apps.googleusercontent.com"
+  // };
+  // if (openLoginIframe?.contentWindow) {
+  //   openLoginIframe.contentWindow.postMessage({ method: "triggerLogin", args, domain: location.origin }, OPEN_LOGIN_URL);
+  // }
+  // return torusdirectsdk.triggerLogin(args);
+  window.location.href = `${OPEN_LOGIN_URL}/start?provider=google`
 }
 
-export const htmlToElement = html => {
-  const template = window.document.createElement("template");
-  const trimmedHtml = html.trim(); // Never return a text node of whitespace as the result
-  template.innerHTML = trimmedHtml;
-  return template.content.firstChild;
-};
+// export const htmlToElement = html => {
+//   const template = window.document.createElement("template");
+//   const trimmedHtml = html.trim(); // Never return a text node of whitespace as the result
+//   template.innerHTML = trimmedHtml;
+//   return template.content.firstChild;
+// };
 
-export function prepareAndAttachIFrame() {
-  openLoginIframe = htmlToElement(
-    `<iframe
-          id="torusIframe"
-          class="torusIframe"
-          src="${OPEN_LOGIN_URL}"
-          style="display: none; position: fixed; top: 0; right: 0; width: 100%;
-          height: 100%; border: none; border-radius: 0;"
-        ></iframe>`
-  );
-  document.body.appendChild(openLoginIframe);
-}
+// export function prepareAndAttachIFrame() {
+//   openLoginIframe = htmlToElement(
+//     `<iframe
+//           id="torusIframe"
+//           class="torusIframe"
+//           src="${OPEN_LOGIN_URL}"
+//           style="display: none; position: fixed; top: 0; right: 0; width: 100%;
+//           height: 100%; border: none; border-radius: 0;"
+//         ></iframe>`
+//   );
+//   document.body.appendChild(openLoginIframe);
+// }
 
-prepareAndAttachIFrame();
+// prepareAndAttachIFrame();
--- examples/vue-app-open-login/src/views/Home.vue
+++ examples/vue-app-open-login/src/views/Home.vue
@@ -2,7 +2,8 @@
   <div class="home">
     <h2>Open Login Demo Application</h2>
     <div :style="{ marginTop: '50px' }">
-      <button @click="login">Login with Google</button>
+      <button v-if="!privKey" @click="login">Login with Google</button>
+      <div v-if="privKey">Login successful</div>
     </div>
   </div>
 </template>
@@ -13,6 +14,18 @@ import { triggerLogin } from "../OpenLoginSdk";
 
 export default {
   name: "Home",
+  data() {
+    return { privKey: "" }
+  },
+  mounted() {
+    const hash = this.$route.hash.slice(1);
+    const { privKey } = hash.split("&").reduce((result, item) => {
+    const [part0, part1] = item.split("=");
+    result[part0] = part1;
+    return result;
+  }, {});
+  this.privKey = privKey
+  },
   methods: {
     async login() {
       await triggerLogin();
